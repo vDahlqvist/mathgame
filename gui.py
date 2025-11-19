85a3fe5f73923ec8f2f554b71341ed19120e9a51
Vilmer Dahlqvist <vilmer.dahlqvist@elev.ntig.se>
2025-11-19 11:05:22 +0100

    added menubar, started work on widget for questions, it is not yet functional

--- gui.py
+++ gui.py
@@ -1,6 +1,30 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMenuBar, QMenu, QWidget, QVBoxLayout, QLabel, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        # bygga GUI här
+       
+        def _createMenuBar(self):
+            menuBar = self.menuBar()
+            # Creating menus using a QMenu object
+            preferencesMenu = QMenu("&Preferences", self)
+            levelMenu = QMenu("&Levels", self)
+            subjectMenu = QMenu("&Subjects", self)
+            startGameMenu = QMenu("&Start Game", self)
+            seeScoresMenu = QMenu("&See Scores", self)
+            # Adding menus to the menu bar
+            menuBar.addMenu(preferencesMenu)
+            menuBar.addMenu(levelMenu)
+            menuBar.addMenu(subjectMenu)
+            menuBar.addMenu(startGameMenu)
+            menuBar.addMenu(seeScoresMenu)
+
+        def _createQuestionArea(self):
+            self.questionWidget = QWidget()
+            layout = QVBoxLayout()
+            layout.addWidget(self.questionWidget)
+
+
+
+        _createMenuBar(self)
+        _createQuestionArea(self)
